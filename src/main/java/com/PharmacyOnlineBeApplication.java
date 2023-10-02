2a8325a3c1457df97c4c01672bd0949e620ed33f
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-10-02 09:56:14 +0700

    up be

---
+++ src/main/java/com/PharmacyOnlineBeApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyOnlineBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyOnlineBeApplication.class, args);
+	}
+
+}
