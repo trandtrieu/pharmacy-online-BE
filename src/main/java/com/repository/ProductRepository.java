419e40a61891fe6a4c2e87a6cc83520b510434f8
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-10-02 10:19:24 +0700

    á

--- src/main/java/com/repository/ProductRepository.java
+++ src/main/java/com/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
