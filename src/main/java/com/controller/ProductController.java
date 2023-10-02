a640b3608ff6936ea51906ecc6a7433ec7d41bb7
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-10-02 10:49:55 +0700

    a

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,16 +1,23 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Product;
+import com.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/pharmacy-online/")
 public class ProductController {
 	
-//	@GetMapping("/products")
-//	public List<Product> getAllProducts(){
-//		return employeeRepository.findAll();
-//	}
+	
+	private ProductRepository productRepository;
+	@GetMapping("/products")
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
 	
 }
